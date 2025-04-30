785f5b9b56791976f0f995f9becc7913927da8e3
helitonns <helitonns@gmail.com>
2025-04-30 19:19:16 -0400

    Componente: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,33 @@
-import { Header } from "../../components/Header"
+import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td>13/04/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighlight variant="outcome">- R$ 100,00</PriceHighlight></td>
+              <td>Alimentação</td>
+              <td>13/04/2025</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   );
 }
